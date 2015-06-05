b63fe038e428ffbdbdc13d7abb8f23c1c3cc1ef1
Vitaly Timofeev <vtim23@gmail.com>
2015-06-05 14:36:46 +0300

    dummy project

---
+++ VTVolumeButtonsObserver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VTVolumeButtonsObserver
+//
+//  Created by Vitaly Timofeev on 05/06/15.
+//  Copyright (c) 2015 Vitaly Timofeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VTVolumeButtonsObserver/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VTVolumeButtonsObserver
+//
+//  Created by Vitaly Timofeev on 05/06/15.
+//  Copyright (c) 2015 Vitaly Timofeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
